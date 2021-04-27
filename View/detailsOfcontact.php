a65c8e1d69c2a069c99823dbcd67e1a6eb67bc52
Abuoblie <Abu.oblie@gmail.com>
2021-04-27 20:48:13 +0200

    final commit

--- View/detailsOfcontact.php
+++ View/detailsOfcontact.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
   require_once "../include/header.php";
 
 
@@ -9,36 +9,39 @@
     $pswd='';
 
     $data =  new Validation();
+    $contact = $data -> getContact('id_People',$_GET['edit']);
     $result = $data -> getInvoice('i.id_People',$_GET['edit']);
-        foreach($result as $row){
- ?>
-<h4 style='text-align:center'>Contact: <?php echo " {$row['first_name']} {$row['last_name']}" ?></h4>
-<h5 style='margin-left:7%'>Contact:  <?php echo " {$row['first_name']} {$row['last_name']}" ?></h5>
-<h5 style='margin-left:7%'>Company: <?php echo $row['name'];?></h5>
-<h5 style='margin-left:7%'>Email: <?php echo $row['email'];?></h5>
-<h5 style='margin-left:7%'>Phone: <?php echo $row['telephone'];?></h5>
+    foreach($contact as $row){
+      echo"<h4 style='text-align:center'> Contact:  {$row['first_name']} {$row['last_name']}</h4>
+           <h5 style='margin-left:7%'> Contact:  {$row['first_name']} {$row['last_name']} </h5>
+           <h5 style='margin-left:7%'> Company: {$row['name']}</h5>
+           <h5 style='margin-left:7%'>Email: {$row['email']}</h5>
+           <h5 style='margin-left:7%'>Phone: {$row['telephone']}</h5>";}
 
-</br></br>
-<h4 style='margin-left:7%'>Contact Person for these invoices:</h4>
 
-<div class="container-md">
-    <table class="table">
-    	<thead>
-    		<tr>
-    			<th>Invoice Number:</th>
-    			<th>Date:</th>
-    		</tr>
-    	</thead>
-    	
-         
-         <tr>
-    		<td><?php echo $row['number']; ?></td>
-    		<td><?php echo $row['invoice_date']; ?></td>
-    		
-    	</tr>
-    	<?php } ?>
-    </table>
-</div>
-<?php
-require_once "../include/footer.php";
+
+    echo "</br></br>
+          <h4 style='margin-left:7%'>Contact Person for these invoices:</h4>
+
+        <div class='container-md'>
+            <table class='table'>
+            	<thead>
+            		<tr>
+            			<th>Invoice Number:</th>
+            			<th>Date:</th>
+            		</tr>
+            	</thead>";
+            	
+              foreach($result as $row){ 
+                echo "<tr>
+            		      <td> {$row['number']}</td>
+            		      <td>{$row['invoice_date']}</td            		
+            	        </tr>";}
+        echo "  </table>
+        </div>";
+
+ require_once "../include/footer.php";       
 ?>
+
+
+
